1c5f25505dddd4b4979d3e72da4e9607e7422ec6
RangerLeonardo <brandonleonardobarrera@gmail.com>
2025-06-08 13:56:27 -0600

    Test in my portfolio

--- api/send_email.js
+++ api/send_email.js
@@ -7,9 +7,8 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 // Esta es la estructura que Vercel espera para sus funciones serverless
 module.exports = async (req, res) => {
-    https://rangerleonardo.github.io/Portafolio/
 
-    res.setHeader('Access-Control-Allow-Origin', 'https://rangerleonardo.github.io');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173/Portafolio/');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
